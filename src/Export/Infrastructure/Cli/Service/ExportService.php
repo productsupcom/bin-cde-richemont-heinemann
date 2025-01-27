8ff1ff5df45376ac943db5e0c4857d98356754c3
khelmrich <kamil.helmrich@productsup.com>
2025-01-28 00:28:38 +0100

    wip: fix tests

--- src/Export/Infrastructure/Cli/Service/ExportService.php
+++ src/Export/Infrastructure/Cli/Service/ExportService.php
@@ -4,29 +4,20 @@ declare(strict_types=1);
 
 namespace Productsup\BinCdeHeinemann\Export\Infrastructure\Cli\Service;
 
-use Productsup\BinCdeHeinemann\Export\Application\Events\DebugContent;
 use Productsup\BinCdeHeinemann\Export\Application\Exporter;
 use Productsup\CDE\Connector\Application\Service\ApplicationService;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Messenger\MessageBusInterface;
-use Throwable;
 
 final class ExportService implements ApplicationService
 {
     public function __construct(
-        private Exporter $exporter,
-        private MessageBusInterface $messageBus
+        private Exporter $exporter
     ) {
     }
 
     public function run(): int
     {
-
-        try {
-            $this->exporter->export();
-        } catch (Throwable $exception) {
-            $this->messageBus->dispatch(new DebugContent($exception->getTraceAsString()));
-        }
+        $this->exporter->export();
 
         return Command::SUCCESS;
     }
