58f67c87bb14113a31eee496504d4a0fa1f2a275
nayan sonawane <nayan.sonawane@productsup.com>
2025-05-12 15:03:24 +0530

    Added timezone

--- tests/Mocks/DateProviderForTests.php
+++ tests/Mocks/DateProviderForTests.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Productsup\BinCdeHeinemann\Tests\Mocks;
 
-use DateTime;
+use DateTimeImmutable;
 use Productsup\BinCdeHeinemann\Export\Application\Provider\DateTimeProvider;
 
 class DateProviderForTests implements DateTimeProvider
 {
     public function getCurrent(string $format): string
     {
-        return (new DateTime('2025-03-11 14:00:00'))->format($format);
+        return (new DateTimeImmutable('2025-03-11 14:00:00'))->format($format);
     }
 }
